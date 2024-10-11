71f4c6080373663ea0d45d56e226ee7c66fc417f
CenterValentine <david@doxy.me>
2024-10-10 20:36:50 -0600

    W2 footer

--- chamber/scripts/getdates.js
+++ chamber/scripts/getdates.js
@@ -1,4 +1,8 @@
 const currentYear = new Date().getFullYear();
 const lastUpdated = new Date().toTimeString();
+const footerSiteInfo = document.getElementById('footer-site-info');
 
-document.getElementById('copywright').innerHTML = `<p>&copy; ${currentYear} David Valentine &#x1F31E St. George, UT &#x1F975</p> <p>Last Updated: ${lastUpdated}</p>`
+console.log(footerSiteInfo);
+let content = footerSiteInfo.innerHTML
+
+footerSiteInfo.innerHTML = `${content}<p>&copy; ${currentYear} David Valentine &#x1F31E</p><p> St. George, UT &#x1F975</p><p>Last Updated: ${lastUpdated}</p>`
